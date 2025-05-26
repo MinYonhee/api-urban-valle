19d258fe8523362e5e3e81484c7e300c7ad3f0d6
Jansen Okasaki <jansencruzyuki@gmail.com>
2025-05-26 19:19:37 -0300

    Consultor (model)

---
+++ imobiliaria/src/main/java/com/realstate/imobiliaria/model/Consultor.java
@@ -0,0 +1,48 @@
+package com.realstate.imobiliaria.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Consultor { 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String setor; 
+    private String telefone;
+
+    public Long getId() {
+        return id; 
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
+
