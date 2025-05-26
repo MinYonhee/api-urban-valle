b7f20145edf05ca0d6c032f2556a22f665b0cd64
Jansen Okasaki <jansencruzyuki@gmail.com>
2025-05-26 19:30:54 -0300

    Consultor (repository)

---
+++ imobiliaria/src/main/java/com/realstate/imobiliaria/repository/ConsultorRepository.java
@@ -0,0 +1,8 @@
+package com.realstate.imobiliaria.repository;
+
+import com.realstate.imobiliaria.model.Consultor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsultorRepository extends JpaRepository<Consultor, Long> {
+}
+
