1112c5b8406bc5fe65c988264aa0e53146a2943a
Jansen Okasaki <jansencruzyuki@gmail.com>
2025-05-27 13:12:39 -0300

    Refinamento Consultor (repository)

--- imobiliaria/src/main/java/com/realstate/imobiliaria/repository/ConsultorRepository.java
+++ imobiliaria/src/main/java/com/realstate/imobiliaria/repository/ConsultorRepository.java
@@ -2,7 +2,10 @@ package com.realstate.imobiliaria.repository;
 
 import com.realstate.imobiliaria.model.Consultor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ConsultorRepository extends JpaRepository<Consultor, Long> {
 }
+
 
