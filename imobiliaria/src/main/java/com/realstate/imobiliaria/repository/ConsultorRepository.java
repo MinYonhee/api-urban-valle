373c9f3621e271272fa8cc81b3103aaf3b2a0ff6
Jansen Okasaki <jansencruzyuki@gmail.com>
2025-06-07 16:30:42 -0300

    Atualizando o Repository de Consultor

--- imobiliaria/src/main/java/com/realstate/imobiliaria/repository/ConsultorRepository.java
+++ imobiliaria/src/main/java/com/realstate/imobiliaria/repository/ConsultorRepository.java
@@ -1,11 +1,12 @@
 package com.realstate.imobiliaria.repository;
 
-import com.realstate.imobiliaria.model.Consultor;
+import com.realstate.imobiliaria.model.consultor.Consultor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ConsultorRepository extends JpaRepository<Consultor, Long> {
+    Optional<Consultor> findByEmail(String email);
 }
-
-
